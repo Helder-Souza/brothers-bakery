fb3728066d30210f45add8959b703605cefc5ff1
Helder-Souza <helder825@hotmail.com>
2025-07-16 15:03:25 -0300

    --amend

--- migrations/create-payments-table.sql
+++ migrations/create-payments-table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS tb_payments (
-	id BINARY(16) default (UUID_TO_BIN(UUID())),
-	order_id BINARY(16) NOT NULL,
-	payment_method VARCHAR(50),
+    id BINARY(16) default (UUID_TO_BIN(UUID())),
+    order_id BINARY(16) NOT NULL,
+    payment_method VARCHAR(50),
     payment_status VARCHAR(50),
     paid_amount DECIMAL(10,2),
     payment_date TIMESTAMP,
-	PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
