3debfff53f6eb8995dbbf4f4cd13f6f0a6ae2e95
Helder-Souza <helder825@hotmail.com>
2025-07-16 10:59:11 -0300

    initial commit

---
+++ src/main/kotlin/com/project/brothers_bakery/BrothersBakeryApplication.kt
@@ -0,0 +1,11 @@
+package com.project.brothers_bakery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrothersBakeryApplication
+
+fun main(args: Array<String>) {
+	runApplication<BrothersBakeryApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/brothers_bakery/BrothersBakeryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.brothers_bakery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BrothersBakeryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
