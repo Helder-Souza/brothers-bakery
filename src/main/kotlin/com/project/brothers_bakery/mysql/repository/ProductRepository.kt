3b17d5c54c0ae142f5300564e03d6ca03d9a1bc8
Helder-Souza <helder825@hotmail.com>
2025-07-17 16:57:10 -0300

    add product impl and tests

---
+++ src/main/kotlin/com/project/brothers_bakery/mysql/repository/ProductRepository.kt
@@ -0,0 +1,28 @@
+package com.project.brothers_bakery.mysql.repository
+
+import com.project.brothers_bakery.domain.Product
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.UUID
+
+interface ProductRepository : JpaRepository<Product, UUID> {
+
+    fun findByProductId(id: UUID): Product?
+
+    @Query("SELECT * FROM tb_products WHERE name = :name", nativeQuery = true)
+    fun findByNameContainingIgnoreCase(name: String): Product
+
+    @Query(
+        "UPDATE tb_products SET " +
+                "name = :name, " +
+                "sku = :sku, " +
+                "ean = :ean, " +
+                "price = :price, " +
+                "url_image = :urlImage, " +
+                "discount = :discount, " +
+                "updated_at = :CURRENT_TIME, " +
+                "shipping = :shipping" +
+                "WHERE product_id = :productId", nativeQuery = true)
+    fun updateProduct(productId: UUID): Product
+
+}
