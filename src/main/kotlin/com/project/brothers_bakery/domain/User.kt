f8de0ed08a968da1c08f7e315443e0b20cb22ce3
valber <valber_05@hotmail.com>
2025-07-21 14:38:08 -0300

    add user impl

---
+++ src/main/kotlin/com/project/brothers_bakery/domain/User.kt
@@ -0,0 +1,23 @@
+package com.project.brothers_bakery.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.sql.Timestamp
+import java.util.UUID
+
+@Entity
+@Table(name = "tb_users")
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID?,
+    val name: String,
+    val email: String,
+    val cpf: String,
+    val password: String,
+    val createdAt: Timestamp,
+    val updatedAt: Timestamp
+)
---
+++ src/test/kotlin/com/project/brothers_bakery/mysql/repositoryImpl/UserRepositoryImplTest.kt
@@ -0,0 +1,63 @@
+package com.project.brothers_bakery.mysql.repositoryImpl
+
+import com.project.brothers_bakery.domain.User
+import com.project.brothers_bakery.dto.UserDto
+import com.project.brothers_bakery.mysql.repository.UserRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import java.sql.Timestamp
+import java.time.LocalDateTime
+import java.util.UUID
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import org.junit.jupiter.api.Test
+
+
+class UserRepositoryImplTest {
+
+    private val userRepository = mockk<UserRepository>()
+    private val userRepositoryImpl = UserRepositoryImpl(userRepository)
+
+    @Test
+    fun `should create a User`() {
+
+        val user = UserDto(
+            id = null,
+            name = "test user",
+            email = "email@email.com",
+            cpf = "11122233344",
+            password = "1234",
+            createdAt = Timestamp.valueOf(LocalDateTime.now()),
+            updatedAt = Timestamp.valueOf(LocalDateTime.now())
+        )
+
+        every { userRepository.save(user.toDomain()) } returns user.toDomain()
+
+        userRepositoryImpl.create(user)
+
+        verify { userRepository.save(user.toDomain()) }
+    }
+
+    @Test
+    fun `should return a User by Id`() {
+        val userId = UUID.randomUUID()
+
+        val user = User(
+            id = userId,
+            name = "test user",
+            email = "email@email.com",
+            cpf = "11122233344",
+            password = "1234",
+            createdAt = Timestamp.valueOf(LocalDateTime.now()),
+            updatedAt = Timestamp.valueOf(LocalDateTime.now())
+        )
+
+        every { userRepository.findByUserId(userId) } returns user
+
+        val result = userRepositoryImpl.findById(userId)
+
+        assertNotNull(result)
+        assertEquals(userId, result.id)
+    }
+}
