0f542eb850bb5e7a6b92ca2a50ffd2f478d2c0c4
valber <valber_05@hotmail.com>
2025-07-22 17:50:26 -0300

    adjust identity from migration

--- migrations/create-products-table.sql
+++ migrations/create-products-table.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS tb_products (
-	product_id BINARY(16) default (UUID_TO_BIN(UUID())),
-	name VARCHAR(50) NOT NULL,
-	sku VARCHAR(255),
-	ean VARCHAR(255),
-	price DECIMAL(10,2) NOT NULL,
-	url_image VARCHAR(255),
-	discount INTEGER,
+    product_id BINARY(16) default (UUID_TO_BIN(UUID())),
+    name VARCHAR(50) NOT NULL,
+    sku VARCHAR(255),
+    ean VARCHAR(255),
+    price DECIMAL(10,2) NOT NULL,
+    url_image VARCHAR(255),
+    discount INTEGER,
     stock INTEGER NOT NULL,
-	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 	PRIMARY KEY (product_id)
 );
