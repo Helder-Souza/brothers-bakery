1cb1751aa989a8d85409c0c6bca6f57a6d8da556
Helder-Souza <helder825@hotmail.com>
2025-07-23 09:59:04 -0300

    add security config to tests

---
+++ src/test/kotlin/com/project/brothers_bakery/config/TestSecurityConfig.kt
@@ -0,0 +1,25 @@
+package com.project.brothers_bakery.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@Profile("test")
+class TestSecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            csrf { disable() }
+            authorizeHttpRequests {
+                authorize(anyRequest, permitAll)
+            }
+        }
+        return http.build()
+    }
+}
+
